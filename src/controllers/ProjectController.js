50eb4d7e522d6a702308d6468af722c4c6985173
Emanuel-Reiter <emanuel.lsrei@gmail.com>
2019-12-06 15:26:17 -0400

    Volta do model de User para o projectController

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -1,5 +1,5 @@
 const Project = require('../models/Project');
-// const User = require('../models/User');
+const User = require('../models/User');
 const mongoose = require('mongoose');
   
 module.exports = {
